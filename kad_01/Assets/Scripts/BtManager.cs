1a7e34edebe8f511b28ceceb608e47dc0d8ad35d
Chu Dinh Cuong <nukniol@gmail.com>
2018-11-29 01:46:48 +0900

    update kad 2

---
+++ kad_01/Assets/Scripts/BtManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BtManager : MonoBehaviour {
+    public Button[] buttons;
+	// Use this for initialization
+	void Start () {
+        // 最初ボタン２押せないように設定
+        ClickEvent(2);
+    }
+    public void ClickEvent(int no)
+    {
+        for (int i = 1; i < buttons.Length; i++)
+        {
+            //  同じ番号を入れたら、押した時、押せないようにする
+            //  同時に同じではないボタンが押せるようにする
+            buttons[i].interactable = i != no;
+        }
+    }
+}
