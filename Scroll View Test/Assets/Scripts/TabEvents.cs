49dfae3e090f978a5cf627cbdf4e5c45e02bcb04
Chu Dinh Cuong <nukniol@gmail.com>
2019-01-10 14:22:21 +0900

    修正

---
+++ Scroll View Test/Assets/Scripts/TabEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class TabEvents : MonoBehaviour {
+
+    [SerializeField]
+    private Color pressedBtColor = Color.blue;
+
+    private Transform tabManager;
+    private Transform tabBts;
+	// Use this for initialization
+	void Start () {
+        tabManager = GameObject.Find("Tab Manager").transform;
+        tabBts = GameObject.Find("Tab Button").transform;
+	}
+	
+	public void ClickEvent(int no)
+    {
+        for(int i = 0; i < tabManager.childCount; i++)
+        {
+            bool flg = i == no;
+            tabManager.GetChild(i).gameObject.SetActive(flg);
+            tabBts.GetChild(i).GetComponent<Image>().color = flg ? pressedBtColor : Color.white;
+        }
+        Debug.Log(name);
+    }
+}
